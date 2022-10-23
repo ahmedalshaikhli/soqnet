9371f6ab684edcba8c40a78f4979b1b72a25afbd
Al Shaikhli <ahmed.al.shaikhli@accenture.com>
2022-10-23 15:29:28 +0200

    End of sections 3

---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IProductRepository
+    {
+        // we do signutre for those two methods
+         Task<Product> GetProductByIdAsync(int id);
+        Task<IReadOnlyList<Product>> GetProductsAsync();
+         Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync();
+         Task<IReadOnlyList<ProductType>> GetProductTypesAsync();
+    }
+}
